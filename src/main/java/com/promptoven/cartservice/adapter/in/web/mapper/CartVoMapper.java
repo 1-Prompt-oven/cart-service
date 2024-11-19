d57c74e7d3e2f7b064a4992bab1f2ea478e1c02f
jaehunLee-git <joz526@naver.com>
2024-11-19 15:38:23 +0900

    Refactor : 장바구니 조회 코드 수정 - GetMapping 시 RequestBody를 쓸 수 없기에 PathVariable 사용 코드로 수정

--- src/main/java/com/promptoven/cartservice/adapter/in/web/mapper/CartVoMapper.java
+++ src/main/java/com/promptoven/cartservice/adapter/in/web/mapper/CartVoMapper.java
@@ -1,58 +1,56 @@
 package com.promptoven.cartservice.adapter.in.web.mapper;
 
-import java.util.List;
-
-import org.springframework.stereotype.Component;
-
 import com.promptoven.cartservice.adapter.in.web.vo.CartCreateRequestVo;
 import com.promptoven.cartservice.adapter.in.web.vo.CartDeleteRequestVo;
-import com.promptoven.cartservice.adapter.in.web.vo.CartGetRequestVo;
 import com.promptoven.cartservice.adapter.in.web.vo.CartResponseVo;
 import com.promptoven.cartservice.adapter.in.web.vo.CartUpdateVo;
 import com.promptoven.cartservice.application.port.in.CartRequestDto;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class CartVoMapper {
 
-	public CartRequestDto toDto(CartCreateRequestVo cartCreateRequestVo) {
-		return CartRequestDto.builder()
-			.memberUuid(cartCreateRequestVo.getMemberUuid())
-			.productUuid(cartCreateRequestVo.getProductUuid())
-			.selected(true)
-			.deleted(false)
-			.build();
-	}
+    public CartRequestDto toDto(CartCreateRequestVo cartCreateRequestVo) {
+        return CartRequestDto.builder()
+                .memberUuid(cartCreateRequestVo.getMemberUuid())
+                .productUuid(cartCreateRequestVo.getProductUuid())
+                .selected(true)
+                .deleted(false)
+                .build();
+    }
 
-	public CartRequestDto toGetDto(CartGetRequestVo cartGetRequestVo) {
-		return CartRequestDto.builder()
-			.memberUuid(cartGetRequestVo.getMemberUuid())
-			.build();
-	}
+    public CartRequestDto toGetDto(String memberUuid) {
+        return CartRequestDto.builder()
+                .memberUuid(memberUuid)
+                .build();
+    }
 
-	public List<CartResponseVo> toVoList(List<CartRequestDto> cartRequestDtoList) {
-		return cartRequestDtoList.stream().map(cartRequestDto -> CartResponseVo.builder()
-			.id(cartRequestDto.getId())
-			.memberUuid(cartRequestDto.getMemberUuid())
-			.productUuid(cartRequestDto.getProductUuid())
-			.selected(cartRequestDto.isSelected())
-			.deleted(cartRequestDto.isDeleted())
-			.createdDate(cartRequestDto.getCreatedDate())
-			.lastModifiedDate(cartRequestDto.getLastModifiedDate())
-			.build()).toList();
-	}
+    public List<CartResponseVo> toVoList(List<CartRequestDto> cartRequestDtoList) {
+        return cartRequestDtoList.stream().map(cartRequestDto -> CartResponseVo.builder()
+                .id(cartRequestDto.getId())
+                .memberUuid(cartRequestDto.getMemberUuid())
+                .productUuid(cartRequestDto.getProductUuid())
+                .selected(cartRequestDto.isSelected())
+                .deleted(cartRequestDto.isDeleted())
+                .createdDate(cartRequestDto.getCreatedDate())
+                .lastModifiedDate(cartRequestDto.getLastModifiedDate())
+                .build()).toList();
+    }
 
-	public CartRequestDto toUpdateDto(CartUpdateVo cartUpdateVo) {
-		return CartRequestDto.builder()
-			.memberUuid(cartUpdateVo.getMemberUuid())
-			.productUuid(cartUpdateVo.getProductUuid())
-			.selected(cartUpdateVo.isSelected())
-			.build();
-	}
+    public CartRequestDto toUpdateDto(CartUpdateVo cartUpdateVo) {
+        return CartRequestDto.builder()
+                .memberUuid(cartUpdateVo.getMemberUuid())
+                .productUuid(cartUpdateVo.getProductUuid())
+                .selected(cartUpdateVo.isSelected())
+                .build();
+    }
 
-	public CartRequestDto toDeleteDto(CartDeleteRequestVo cartDeleteRequestVo) {
-		return CartRequestDto.builder()
-			.memberUuid(cartDeleteRequestVo.getMemberUuid())
-			.productUuid(cartDeleteRequestVo.getProductUuid())
-			.build();
-	}
+    public CartRequestDto toDeleteDto(CartDeleteRequestVo cartDeleteRequestVo) {
+        return CartRequestDto.builder()
+                .memberUuid(cartDeleteRequestVo.getMemberUuid())
+                .productUuid(cartDeleteRequestVo.getProductUuid())
+                .build();
+    }
 }
