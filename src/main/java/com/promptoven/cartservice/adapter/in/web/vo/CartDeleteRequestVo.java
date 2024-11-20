96cc5b0d9d8e6198c938f03d44ae669366e13e16
jaehunLee-git <joz526@naver.com>
2024-11-20 11:13:37 +0900

    Refactor : Json 역직렬화 문제 해결

--- src/main/java/com/promptoven/cartservice/adapter/in/web/vo/CartDeleteRequestVo.java
+++ src/main/java/com/promptoven/cartservice/adapter/in/web/vo/CartDeleteRequestVo.java
@@ -1,10 +1,15 @@
 package com.promptoven.cartservice.adapter.in.web.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CartDeleteRequestVo {
+
     private Long cartId;
 }
