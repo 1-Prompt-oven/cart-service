9a760c9e586db4d3f17a9f6b558e8df2d30316d6
jaehunLee-git <joz526@naver.com>
2024-11-28 13:46:03 +0900

    Refactor : 엔티티 access level 수정

--- src/main/java/com/promptoven/cartservice/adapter/mysql/entity/CartEntity.java
+++ src/main/java/com/promptoven/cartservice/adapter/mysql/entity/CartEntity.java
@@ -1,38 +1,38 @@
 package com.promptoven.cartservice.adapter.mysql.entity;
 
-import java.time.LocalDateTime;
-
 import com.promptoven.cartservice.global.common.entity.BaseEntity;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Entity
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CartEntity extends BaseEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	private String memberUuid;
-	private String productUuid;
-	private boolean selected;
-	private boolean deleted;
+    private String memberUuid;
+    private String productUuid;
+    private boolean selected;
+    private boolean deleted;
 
-	@Builder
-	public CartEntity(Long id, String memberUuid, String productUuid, boolean selected, boolean deleted,
-		LocalDateTime createdAt) {
-		this.id = id;
-		this.memberUuid = memberUuid;
-		this.productUuid = productUuid;
-		this.selected = selected;
-		this.deleted = deleted;
-	}
+    @Builder
+    public CartEntity(Long id, String memberUuid, String productUuid, boolean selected, boolean deleted,
+                      LocalDateTime createdAt) {
+        this.id = id;
+        this.memberUuid = memberUuid;
+        this.productUuid = productUuid;
+        this.selected = selected;
+        this.deleted = deleted;
+    }
 }
